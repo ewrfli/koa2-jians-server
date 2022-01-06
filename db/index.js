52aaf04705f266da9089f7758da948b4a2a7d420
ewrf3 <q304062@163.com>
2022-01-07 00:32:45 +0800

    project init

---
+++ db/index.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+module.exports = () => {
+    mongoose
+        .connect("mongodb://localhost:27017/jianshu", {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+        })
+        .then(() => {
+            console.log("数据库连接成功");
+        })
+        .catch((err) => {
+            console.error("数据库连接失败", err);
+        });
+};
