2388063e1c672abeea72ebaface854143657f951
ewrfli <q304062@163.com>
2022-01-10 16:55:48 +0800

    fix：koa-multer

--- routes/upload.js
+++ routes/upload.js
@@ -8,33 +8,44 @@ const protectedUploadRouter = new Router({ //有保护的
     prefix: '/upload'
 });
 
-let upload = multer({ //中间件
-    //设置文件存储位置
-    // destination: function(req, file, cb){
-    //   let date = new Date()
-    //   let year = date.getFullYear()
-    //   let month = date.getMonth() + 1
-    //   let day = date.getDate()
-    //   let dir = "/public/uploads/"// + year + month + day 
-    //   //判断目录是否存在
-    //   if(!fs.existsSync(dir)){
-    //       fs.mkdirSync(dir, {
-    //           recursive: true //递归？
-    //       })
-    //   }  
+//     // // //文件名称
+//     // filename: function(req, file, cb){
+//     //     // let fileName = file.filename + '-' + Date.now() + path.extname(file.originalname)
+//     //     let fileFormat = (file.originalname).split(".");
+//     //     cb(null,Date.now() + "." + fileFormat[fileFormat.length - 1]);
+//     //     // cb(null, fileName) //设置上传文件的名字
+//     // }
+// })
 
-    //   cb(null, path.join(__dirname, 'public/uploads/')) //文件上传到目录
-    // },
-    // //文件名称
-    // filename: function(req, file, cb){
-    //     // let fileName = file.filename + '-' + Date.now() + path.extname(file.originalname)
-    //     let fileFormat = (file.originalname).split(".");
-    //     cb(null,Date.now() + "." + fileFormat[fileFormat.length - 1]);
-    //     // cb(null, fileName) //设置上传文件的名字
-    // }
+let storage = multer.diskStorage({
+    // 文件保存路径
+    destination: function (req, file, cb) {
+      let date = new Date()
+      let year = date.getFullYear()
+      let month = date.getMonth() + 1
+      if(month < 10){
+          month = '0'+String(month)
+      }
+      let day = date.getDate()
+      let dir = "public/uploads/" + year + month + day 
+      //判断目录是否存在
+      if(!fs.existsSync(dir)){
+          fs.mkdirSync(dir, {
+              recursive: true //递归？
+          })
+      }  
+      cb(null, dir) //文件上传到目录
+    },
 
-    dest: 'public/uploads/'
+    filename: function(req, file, cb){
+        let fileName = file.fieldname + '-' + Date.now() + '-' + file.originalname
+        cb(null, fileName) //设置上传文件的名字
+    }
 })
+
+//加载配置
+let upload = multer({ storage: storage })
+
 
 protectedUploadRouter.post("/img", upload.single('myfile'), async ctx => { //myfile字段 //body form-data
     ctx.body = {
