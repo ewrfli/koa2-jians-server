0ad3341501e520df6eb3b1dda5f2ca6bf6c175dc
ewrfli <q304062@163.com>
2022-01-20 18:33:17 +0800

    fix：fix

--- routes/comment.js
+++ routes/comment.js
@@ -9,6 +9,6 @@ commentRouter.post("/add", commentCtl.commentAdd);
 commentRouter.post("/findone", commentCtl.commentFindOne);
 commentRouter.post("/findall", commentCtl.commentFindAll);//后台文章查询 可以根据。。。
 commentRouter.post("/web/findall", commentCtl.commentFindAll);//前台评论查询 根据文章id查询 /"articleId": "10101"
-commentRouter.delete("/del", commentCtl.commentDel);
+commentRouter.post("/del", commentCtl.commentDel);
 commentRouter.post("/update", commentCtl.commentUpdate);
 module.exports = commentRouter
